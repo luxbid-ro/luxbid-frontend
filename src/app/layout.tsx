1c8ab876fa1b32b8605f38748c0cbc655d7b010b
Andrei Ionut Tufa <andrei.ionut91@icloud.com>
2025-09-02 23:34:05 +0300

    Revert "fix: remove LazyAgeVerificationModal from layout.tsx"
    This reverts commit 514b57102aec6cced9e1b4837e97c929883c9f49.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ import ErrorBoundary from '@/components/ErrorBoundary'
 import ConditionalAuth from '@/components/ConditionalAuth'
 import { 
   LazyCookieBanner, 
-  LazyAccessibilityWidget
+  LazyAccessibilityWidget, 
+  LazyAgeVerificationModal 
 } from '@/components/LazyComponents'
 import UpdateNotification from '@/components/UpdateNotification'
 import { OrganizationSchema, WebsiteSchema } from '@/components/StructuredData'
@@ -97,6 +98,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </main>
             <LazyCookieBanner />
             <LazyAccessibilityWidget />
+            <LazyAgeVerificationModal />
             <UpdateNotification />
             
             {/* Global Structured Data */}
